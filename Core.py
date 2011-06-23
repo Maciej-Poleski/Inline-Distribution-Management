c8b4571cbc01dd039a4f0c8886695c97169b00c2
Maciej Poleski <Maciej-Poleski@users.noreply.github.com>
2011-06-23 15:07:55 +0200

    Biblioteka w tej wersji potrafi odczytać plik nagłówkowy z szablonu.

--- Core.py
+++ Core.py
@@ -2,6 +2,7 @@ from PyQt4.QtCore import *
 
 class Model(QAbstractTableModel):
     def __init__(self, directory):
+        QAbstractTableModel.__init__(self)
         self.header=None
         file=QFile(directory+'/template.xml')
         file.open(QIODevice.ReadOnly)
@@ -12,6 +13,29 @@ class Model(QAbstractTableModel):
         if self.header==None:
             raise Exception('Podana ścieżka nie zawiera poprawnego szablonu!')
     
+    def headerData(self, section, orientation, role=Qt.DisplayRole):
+        '''Prototyp funkcji zwracajcej nagłówek. Jedyna obsługiwana rola to Qt.DisplayRole'''
+        if role == Qt.DisplayRole:
+            if orientation == Qt.Horizontal:
+                return self.header[section]     # FIXME:Trzeba by sprawdzić, czy nie wychodzimy poza zakres
+        return None
+    
+    def columnCount(self, parent=QModelIndex()):
+        '''Prototyp funkcji zwracajcej liczbę kolumn. Parametr parent jest ignorowany'''
+        return len(self.header)
+    
+    def rowCount(self, parent=QModelIndex()):
+        '''Prototyp funkcji zwracajcej liczbę wierszy. Parametr parent jest ignorowany'''
+        return 0    #FIXME: Prawdziwa implementacja musi użyć prawdziwej liczby wierszy!
+    
+    def parent(self, index):
+        '''STUB. Nie wiem do czego ta metoda tak naprawdę służy.'''
+        return QModelIndex()
+    
+    def data(self, index, role=Qt.DisplayRole):
+        '''STUB. FIXME: Potrzebna jest implementacja.'''
+        return None
+
     def __parse_template_document(self, xmlReader):
         while xmlReader.atEnd()==False:
             state=xmlReader.readNext()
@@ -28,4 +52,3 @@ class Model(QAbstractTableModel):
                 self.header.append(xmlReader.name())
             elif state == QXmlStreamReader.EndElement and xmlReader.name() == 'header':
                 break
-            
--- __init__.py
+++ __init__.py
@@ -1,5 +1,9 @@
 if __name__ == "__main__":
     from Core import *
     from PyQt4.QtCore import *
-    x=Model('C:\\Users\\evil\\Projekty\\Inline Distribution Management')
-    print(x.header)
+    from PyQt4.QtGui import *
+    app=QApplication([])
+    view=QTableView()
+    view.setModel(Model('C:\\Users\\evil\\Projekty\\Inline Distribution Management'))
+    view.show();
+    app.exec_()
