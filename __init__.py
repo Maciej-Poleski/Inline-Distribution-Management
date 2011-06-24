f7ae89dd9c2ee17055b95d15900e5e56b1934f02
Maciej Poleski <Maciej-Poleski@users.noreply.github.com>
2011-06-24 16:33:54 +0200

    Usuwam starter. Nie jest on już potrzebny.

--- __init__.py
+++
@@ -1,9 +0,0 @@
-if __name__ == "__main__":
-    from Core import *
-    from PyQt4.QtCore import *
-    from PyQt4.QtGui import *
-    app=QApplication([])
-    view=QTableView()
-    view.setModel(Model('C:\\Users\\evil\\Projekty\\Inline Distribution Management'))
-    view.show();
-    app.exec_()
